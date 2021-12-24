4117c447dd442b68ec313c5c8b7100519df7ed21
Thomas Dwi <awaka.19051@mhs.its.ac.id>
2021-12-24 16:01:45 +0800

    fix hapus bug

--- hapus.php
+++ hapus.php
@@ -2,18 +2,18 @@
 
 include("config.php");
 
-if( isset($_GET['NIS']) ){
+if( isset($_GET['id']) ){
 
     // ambil id dari query string
-    $id = $_GET['NIS'];
+    $id = $_GET['id'];
 
     // buat query hapus
-    $sql = "DELETE FROM calon_siswa WHERE id=$id";
+    $sql = "DELETE FROM siswa WHERE id=$id";
     $query = mysqli_query($koneksi, $sql);
 
     // apakah query hapus berhasil?
     if( $query ){
-        header('Location: list-siswa.php');
+        header('Location: index.php');
     } else {
         die("gagal menghapus...");
     }
